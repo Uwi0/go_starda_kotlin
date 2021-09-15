c9283c574aab691452876114102c2c6a3318e6ee
Dwi Prasetyo <kakapoweb@gmail.com>
2021-09-15 15:29:55 +0700

    initial commit before disaster

--- app/src/main/java/com/kakapo/gostrada/MainActivity.kt
+++ app/src/main/java/com/kakapo/gostrada/MainActivity.kt
@@ -2,10 +2,32 @@ package com.kakapo.gostrada
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.kakapo.gostrada.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        setSupportActionBar(mBinding.contentMain.customAppbar.toolbar)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+
+        val navView: BottomNavigationView = mBinding.contentMain.bottomNavigation.navView
+
+        navController = findNavController(R.id.nav_host)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
